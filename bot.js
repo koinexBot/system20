febed6ca9c9362056101a26025129acf83009cff
koinexBot <46602174+koinexBot@users.noreply.github.com>
2019-01-15 12:33:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3031,42 +3031,7 @@ By Koinex Team ( imad )
 `);
 
 });
-
-client.on('ready', function(){
-
-  client.user.setStatus("dnd");
-
-    var ms = 100 ;	
-
-    var setGame = ['!inv','!help'];	
-
-    var i = -1;	
-
-    var j = 0;	
-
-    setInterval(function (){	
-
-        if( i == -1 ){	
-
-j = 1;	
-
-       }	
-
-        if( i == (setGame.length)-1 ){	
-
-            j = -1;	
-
-      }	
-
-       i = i+j;	
-
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);	
-
-}, ms);	
-
-	
-
-});
+client.on('ready', functi
 
 
 //Koinex Team END NOW THIS IS END
